de6f238a24c1d3f4d56e0137e69fb91bbeea70cf
Wendy D. Anderson <wdander@pacbell.net>
2018-06-05 00:22:18 -0700

    Update script.js
    updated file with j query for counting responses. code not working yet. still under construction.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-// Waits for DOM to be fully loaded.
-$( document ).ready( function () {
-
-var array
-  });
-
+/*global*/
+//begin jquery to start the process
+$(document).ready(function() {
+//when the submit button is clicked begin the check function
+    $("#submit").click(function(){
+     check(); 
+    });
 });
+
+//the check function will run and put all the yes inputs in an array
+function check() {
+//
+var question = $("#quiz input[type=radio]");
+  console.log(question);
+   //for (i = 0; i < question.length; i++); {
+   //if ( question[i].is(':checked') ) { console.log (checked) }
+ 
+  // }
+
+}
+
+
+
+    
